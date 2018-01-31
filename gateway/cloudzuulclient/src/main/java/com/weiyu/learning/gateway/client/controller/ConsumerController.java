6edf4a289deba7e22a93b466b4d7eb90ee8fdcdc
weiyu <weiyu@ctrip.com>
2018-01-31 10:16:37 +0800

    修改 适配 eclipse 开发工具

--- gateway/cloudzuulclient/src/main/java/com/weiyu/learning/gateway/client/controller/ConsumerController.java
+++ gateway/cloudzuulclient/src/main/java/com/weiyu/learning/gateway/client/controller/ConsumerController.java
@@ -1,8 +1,10 @@
-package com.weiyu.learning.gateway.client;
+package com.weiyu.learning.gateway.client.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.weiyu.learning.gateway.client.CalculatorService;
 
 import javax.annotation.Resource;
 
